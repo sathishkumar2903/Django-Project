a6a8dedad72fe7dcb264c3662f813bd8d5212e21
sathishkumar2903 <sathish15963@gmail.com>
2026-03-03 16:19:27 +0530

    added STATIC_ROOT

--- myapp/settings.py
+++ myapp/settings.py
@@ -138,3 +138,9 @@ STATICFILES_DIRS = [
 #https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
